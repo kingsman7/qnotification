fa2696e6e8ae84acd6272227e715e260bc92a4cb
Yeison Tapia <yftapial@ut.edu.co>
2019-09-03 18:33:54 -0500

    + Add implement in notification widget component

--- _components/widgets/notifications.vue
+++ _components/widgets/notifications.vue
@@ -1,45 +1,108 @@
 <template>
-  <div>
-    <q-btn
-      flat
-      dense
-      @click="showNotifications = !showNotifications"
-      icon="notifications"/>
-    <div v-if="showNotifications">
-      <q-list
-        highlight>
+<div>
+  <div class="relative-position q-mr-md">
+    <q-chip
+      round
+      text-color="black"
+      color="white"
+      floating>{{notifications.pagination.total}}</q-chip>
+  </div>
+  <q-btn-dropdown
+    dense
+    outline
+    rounded
+    class="q-mr-lg"
+    icon="notifications">
+    <q-scroll-area
+      style="width: 400px;height:200px;">
+    <q-list separator class="q-pr-sm">
+      <div v-if="notifications.data.length">
         <q-item
-          link
-          :to="{name: notificaction.link}"
-          v-for="(notificaction, index) in notificactions.data"
+          v-for="(notification, index) in notifications.data"
           :key="index">
           <q-item-side
-            :icon="notificaction.icon_class" />
+            color="info"
+            :icon="notification.icon_class" />
           <q-item-main
-            :label="notificaction.title"
-            :sublabel="notificaction.message"/>
+            :label="notification.title"
+            :sublabel="notification.message"/>
           <q-item-side
             right
-            :stamp="notificaction.timeAgo" />
+            :stamp="notification.timeAgo" />
+          <q-item-side right >
+            <q-btn
+              inverted
+              color="primary"
+              outline
+              dense
+              flat
+              round
+              @click="$router.push({name: notification.link})"
+              size="10px"
+              icon="fas fa-external-link-alt"/>
+          </q-item-side>
+          <q-item-side right >
+            <q-btn
+              inverted
+              color="primary"
+              outline
+              flat
+              dense
+              round
+              @click="updateNotification(notification)"
+              size="15px"
+              icon="far fa-times-circle"/>
+          </q-item-side>
         </q-item>
-      </q-list>
-    </div>
-  </div>
+      </div>
+      <div v-else>
+        <q-item>
+          <q-item-side></q-item-side>
+          <q-item-main>
+            There are no notifications
+          </q-item-main>
+        </q-item>
+        <q-item>
+          <q-item-side></q-item-side>
+          <q-item-main>
+            <q-btn label="view All" class="full-width"></q-btn>
+          </q-item-main>
+        </q-item>
+      </div>
+    </q-list>
+    </q-scroll-area>
+  </q-btn-dropdown>
+</div>
 </template>
 
 <script>
+  import Echo from "laravel-echo";
   export default {
     data () {
       return {
         loading: false,
-        showNotifications: true,
-        notificactions: {
-          data: []
-        }
+        showNotifications: false,
+        notifications: {
+          data: [],
+          pagination:{
+            page: 1,
+            take: 10,
+            total: 0,
+          }
+        },
+        echo: null
+      }
+    },
+    computed:{
+      unreadNotifications (){
+        return 0
       }
     },
     created() {
-      this.getNotifications()
+      this.$nextTick(()=> {
+        this.getNotifications()
+        this.initPusher()
+      })
     },
     methods: {
       getNotifications(){
@@ -48,23 +111,55 @@
           params: {
             filter: {
               me: true
-            }
-          }
+            },
+            page: this.notifications.pagination.page,
+            take: this.notifications.pagination.take
+          },
         }
         this.$crud.index('apiRoutes.qnotification.notifications', params)
           .then( response => {
-            this.notificactions.data = response.data
+            this.notifications.data = response.data
+            this.notifications.pagination.total = response.meta.page.total
             this.loading = false
           })
           .catch( error => {
             this.loading = false
             this.$alert.error({message: this.$tr('ui.message.formInvalid'), pos: 'bottom'})
           })
-      }
+      },
+      updateNotification(notification){
+        notification.isReas = true
+        this.loading = true
+        let params = {params: {}}
+        this.$crud.update('apiRoutes.qnotification.notifications', notification.id, notification, params)
+          .then( response => {
+            // Update notification in Local data
+            this.notifications.pagination.total --
+            this.loading = false
+          })
+          .catch( error => {
+            console.warn(error)
+            this.$alert.error({message: this.$tr('ui.message.formInvalid'), pos: 'bottom'})
+            this.loading = false
+          })
+      },
+      initPusher(){
+        this.echo = new Echo({
+          broadcaster: env('BROADCAST_DRIVER', 'pusher'),
+          key: env('PUSHER_APP_KEY'),
+          cluster: env('PUSHER_APP_CLUSTER'),
+          encrypted: env('PUSHER_APP_ENCRYPTED'),
+        })
+        this.echo.channel('global')
+          .listen(`.notification.new`, data => {
+            this.notifications.data.unshift(data)
+            this.notifications.pagination.total ++
+          })
+          .listen(`.notification.new.${this.$store.state.quserAuth.userData.id}`, data => {
+            this.notifications.data.unshift(data)
+            this.notifications.pagination.total ++
+          })
+      },
     }
   }
 </script>
-
-<style scoped>
-
-</style>
