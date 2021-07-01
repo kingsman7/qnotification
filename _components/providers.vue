36ab1b737a76fa6a6f41117e7da02a00f6dcdeb5
msolanogithub <msolano2505@gmail.com>
2021-07-01 15:35:10 -0500

    +Update: Implement `pageActions` component to pages

--- _components/providers.vue
+++ _components/providers.vue
@@ -5,23 +5,22 @@
       <!--Providers list-->
       <div class="row q-col-gutter-md">
         <div class="col-12 col-sm-6 col-md-4 col-lg-3" v-for="(provider, key) in providers" :key="key">
-          <q-card class="my-card">
+          <q-card class="my-card box box-auto-height" style="padding: 0">
             <q-card-section class="text-h5 text-white row items-center q-py-lg"
                             :style="`background-color: ${provider.color}`">
               <!--Icon-->
               <q-icon :name="provider.icon" class="q-mr-md"/>
               <!--Name-->
-              <label>{{provider.name}}</label>
+              <label>{{ provider.name }}</label>
             </q-card-section>
 
             <q-card-actions>
+              <!--Status-->
+              <label :class="`text-${providerStatus(provider).color}`">{{ providerStatus(provider).label }}</label>
+              <q-space/>
               <!--Setting-->
               <q-btn outline color="primary" rounded dense no-caps :label="$tr('ui.label.setup')"
                      @click="modal.provider = provider, modal.show = true"/>
-              <q-space/>
-              <!--Status-->
-              <q-chip :color="providerStatus(provider).color" text-color="white"
-                      :label="providerStatus(provider).label"/>
             </q-card-actions>
           </q-card>
         </div>
@@ -36,7 +35,7 @@
               <!--Icon-->
               <q-icon :name="modal.provider.icon" class="q-mr-md"/>
               <!--Name-->
-              <label>{{modal.provider.name}}</label>
+              <label>{{ modal.provider.name }}</label>
             </q-toolbar-title>
             <q-btn flat v-close-popup icon="fas fa-times"/>
           </q-toolbar>
@@ -75,148 +74,148 @@
   </div>
 </template>
 <script>
-  export default {
-    beforeDestroy() {
-      this.$root.$off('page.data.refresh')
+export default {
+  beforeDestroy() {
+    this.$root.$off('page.data.refresh')
+  },
+  props: {},
+  components: {},
+  watch: {},
+  mounted() {
+    this.$nextTick(function () {
+      this.init()
+    })
+  },
+  data() {
+    return {
+      loading: false,
+      providers: [],
+      modal: {
+        show: false,
+        loading: false,
+        provider: false,
+        form: {}
+      }
+    }
+  },
+  computed: {
+    providerFields() {
+      let fields = this.modal.provider.fields || {}
+      let dataProvider = this.modal.provider.data || {}
+
+      for (var fieldName in fields) {
+        let field = fields[fieldName]
+        //Add fake field to form
+        if (field.isFakeField) {
+          let nameFakeField = this.getNameFakeField(field)
+          if (!this.modal.form[nameFakeField]) this.modal.form[nameFakeField] = {}
+          //Add fake field value
+          if (dataProvider[nameFakeField] && (dataProvider[nameFakeField][fieldName] !== undefined))
+            fields[fieldName].value = dataProvider[nameFakeField][fieldName]
+        }
+        //Add data to field value
+        else {
+          fields[fieldName].value = dataProvider[fieldName] || field.value || null
+        }
+      }
+
+      return fields
+    }
+  },
+  methods: {
+    init() {
+      //Get providers
+      this.getProviders()
+      //Listen refresh page
+      this.$root.$on('page.data.refresh', () => this.getProviders(true))
     },
-    props: {},
-    components: {},
-    watch: {},
-    mounted() {
-      this.$nextTick(function () {
-        this.init()
+
+    //Get providers
+    getProviders(refresh = false) {
+      return new Promise((resolve, reject) => {
+        this.loading = true//Loading
+
+        // Request data
+        let requestData = {refresh: refresh}
+
+        //Request
+        this.$crud.index('apiRoutes.qnotification.providers', requestData).then(response => {
+          this.providers = response.data
+          this.loading = false
+          resolve(response.data)
+        }).catch(error => {
+          this.loading = false
+          reject(error)
+        })
       })
     },
-    data() {
-      return {
-        loading: false,
-        providers: [],
-        modal: {
-          show: false,
-          loading: false,
-          provider: false,
-          form: {}
-        }
-      }
+
+    //Return name fakeField
+    getNameFakeField(field) {
+      return (typeof field.isFakeField == 'string') ? field.isFakeField : 'options'
     },
-    computed: {
-      providerFields() {
-        let fields = this.modal.provider.fields || {}
-        let dataProvider = this.modal.provider.data || {}
 
-        for (var fieldName in fields) {
-          let field = fields[fieldName]
-          //Add fake field to form
-          if (field.isFakeField) {
-            let nameFakeField = this.getNameFakeField(field)
-            if (!this.modal.form[nameFakeField]) this.modal.form[nameFakeField] = {}
-            //Add fake field value
-            if (dataProvider[nameFakeField] && (dataProvider[nameFakeField][fieldName] !== undefined))
-              fields[fieldName].value = dataProvider[nameFakeField][fieldName]
-          }
-          //Add data to field value
-          else {
-            fields[fieldName].value = dataProvider[fieldName] || field.value || null
-          }
-        }
-
-        return fields
+    //Return config to show provider status
+    providerStatus(provider) {
+      //Default response [Disabled]
+      let response = {
+        color: 'red',
+        label: this.$tr('ui.label.disabled')
       }
-    },
-    methods: {
-      init() {
-        //Get providers
-        this.getProviders()
-        //Listen refresh page
-        this.$root.$on('page.data.refresh', () => this.getProviders(true))
-      },
 
-      //Get providers
-      getProviders(refresh = false) {
-        return new Promise((resolve, reject) => {
-          this.loading = true//Loading
-
-          // Request data
-          let requestData = {refresh: refresh}
-
-          //Request
-          this.$crud.index('apiRoutes.qnotification.providers', requestData).then(response => {
-            this.providers = response.data
-            this.loading = false
-            resolve(response.data)
-          }).catch(error => {
-            this.loading = false
-            reject(error)
-          })
-        })
-      },
-
-      //Return name fakeField
-      getNameFakeField(field) {
-        return (typeof field.isFakeField == 'string') ? field.isFakeField : 'options'
-      },
-
-      //Return config to show provider status
-      providerStatus(provider) {
-        //Default response [Disabled]
-        let response = {
-          color: 'grey-7',
-          label: this.$tr('ui.label.disabled')
+      //If prvider is enabled
+      if (provider && provider.data && parseInt(provider.data.status))
+        response = {
+          color: 'green',
+          label: this.$tr('ui.label.enabled')
         }
 
-        //If prvider is enabled
-        if (provider && provider.data && parseInt(provider.data.status))
-          response = {
-            color: 'green',
-            label: this.$tr('ui.label.enabled')
-          }
+      //Response
+      return response
+    },
 
-        //Response
-        return response
-      },
+    //Save config provider
+    saveProviderConfig() {
+      this.modal.loading = true
+      let provider = this.$clone(this.modal.provider)//Provider
+      //Form data
+      let formData = this.$clone({...this.modal.form, systemName: provider.systemName, name: provider.name})
+      let providerId = (provider.data && provider.data.id) ? provider.data.id : false//Provider ID
 
-      //Save config provider
-      saveProviderConfig() {
-        this.modal.loading = true
-        let provider = this.$clone(this.modal.provider)//Provider
-        //Form data
-        let formData = this.$clone({...this.modal.form, systemName: provider.systemName, name: provider.name})
-        let providerId = (provider.data && provider.data.id) ? provider.data.id : false//Provider ID
+      //Method after save provider
+      const afterSaveProvider = () => {
+        //Get providers data
+        this.getProviders(true)
+        //Order modal
+        this.modal.show = false
+        this.modal.loading = false
+      }
 
-        //Method after save provider
-        const afterSaveProvider = () => {
-          //Get providers data
-          this.getProviders(true)
-          //Order modal
-          this.modal.show = false
+      //update provider
+      if (providerId) {
+        //Request
+        this.$crud.update('apiRoutes.qnotification.providers', providerId, formData).then(response => {
+          afterSaveProvider()//Get providers data
+          this.$alert.info({message: this.$tr('ui.message.recordCreated')})//Show message
+        }).catch(error => {
           this.modal.loading = false
-        }
-
-        //update provider
-        if (providerId) {
-          //Request
-          this.$crud.update('apiRoutes.qnotification.providers', providerId, formData).then(response => {
-            afterSaveProvider()//Get providers data
-            this.$alert.info({message: this.$tr('ui.message.recordCreated')})//Show message
-          }).catch(error => {
-            this.modal.loading = false
-            this.$alert.error({message: this.$tr('ui.message.recordNoUpdated')})
-          })
-        }
-        //Create provider
-        else {
-          //Request
-          this.$crud.create('apiRoutes.qnotification.providers', formData).then(response => {
-            afterSaveProvider()//Get providers data
-            this.$alert.info({message: this.$tr('ui.message.recordCreated')})//Show message
-          }).catch(error => {
-            this.modal.loading = false
-            this.$alert.error({message: this.$tr('ui.message.recordNoCreated')})
-          })
-        }
+          this.$alert.error({message: this.$tr('ui.message.recordNoUpdated')})
+        })
+      }
+      //Create provider
+      else {
+        //Request
+        this.$crud.create('apiRoutes.qnotification.providers', formData).then(response => {
+          afterSaveProvider()//Get providers data
+          this.$alert.info({message: this.$tr('ui.message.recordCreated')})//Show message
+        }).catch(error => {
+          this.modal.loading = false
+          this.$alert.error({message: this.$tr('ui.message.recordNoCreated')})
+        })
       }
     }
   }
+}
 </script>
 <style lang="stylus">
 </style>
--- _pages/admin/setting/index.vue
+++ _pages/admin/setting/index.vue
@@ -4,12 +4,10 @@
     <div id="contentConfig" class="row q-col-gutter-y-xl">
       <!--Providers-->
       <div id="contentProviders" class="col-12">
-        <!--Title-->
-        <div class="text-h6 text-grey-9">
-          <q-icon name="fab fa-stack-exchange" class="q-mr-xs" />
-          {{$trp('qnotification.layout.label.provider')}}
+        <!--Page Actions-->
+        <div class="box box-auto-height q-mb-md">
+          <page-actions :title="`${$tr($route.meta.headerTitle)} | ${$trp('qnotification.layout.label.provider')}`"/>
         </div>
-        <q-separator class="q-mb-md q-mt-xs" />
         <!--Component-->
         <providers/>
       </div>
@@ -17,10 +15,10 @@
       <div id="contentRules" class="col-12" v-if="false">
         <!--Title-->
         <div class="text-h6 text-grey-9">
-          <q-icon name="fas fa-directions" class="q-mr-xs" />
-          {{$trp('qnotification.layout.label.rule')}}
+          <q-icon name="fas fa-directions" class="q-mr-xs"/>
+          {{ $trp('qnotification.layout.label.rule') }}
         </div>
-        <q-separator class="q-mb-md q-mt-xs" />
+        <q-separator class="q-mb-md q-mt-xs"/>
         <!--Component-->
         <rules/>
       </div>
@@ -28,24 +26,24 @@
   </q-page>
 </template>
 <script>
-  //Components
-  import providers from '@imagina/qnotification/_components/providers'
-  import rules from '@imagina/qnotification/_components/rules'
+//Components
+import providers from '@imagina/qnotification/_components/providers'
+import rules from '@imagina/qnotification/_components/rules'
 
-  export default {
-    props: {},
-    components: {providers, rules},
-    watch: {},
-    mounted() {
-      this.$nextTick(function () {
-      })
-    },
-    data() {
-      return {}
-    },
-    computed: {},
-    methods: {}
-  }
+export default {
+  props: {},
+  components: {providers, rules},
+  watch: {},
+  mounted() {
+    this.$nextTick(function () {
+    })
+  },
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
 </script>
 <style lang="stylus">
 </style>
